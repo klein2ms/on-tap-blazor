08ee144205a292d318e6ae60ecf2f2cb50c1f24f
Sean Klein <klein2ms@gmail.com>
2019-05-10 23:14:52 -0400

    :tada: initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# on-tap-blazor
+Blazor implementation of the on-tap fantasy football api
---
+++ on-tap-blazor/Player.cs
@@ -0,0 +1,11 @@
+﻿namespace on_tap_blazor
+{
+    public class Player
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string PositionShortName { get; set; }
+        public decimal Adp { get; set; }
+        public string TeamName { get; set; }
+    }
+}
